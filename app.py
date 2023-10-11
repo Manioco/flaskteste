9b74926b5b13b06be2a24ba5efd165aa2d86f81e
Manioco <prausartudo@gmail.com>
2023-10-11 15:28:39 -0300

    commit

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Creates the database if it doesn't exist
         print("Database created!")
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
